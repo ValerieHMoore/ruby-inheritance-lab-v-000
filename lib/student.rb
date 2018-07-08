daa5763f1b7aaaec37316c1dccc40653b7a0f5a7
Valerie Moore <periwinkle17@outlook.com>
2018-07-08 16:53:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
 
+  def initialize
+    @knowledge = []
+  end
+  
+  def learn(string)
+    @knowledge << string
+  end
+  
+  def knowledge
+    self.knowledge
+  end
+  
 end
